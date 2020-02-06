fda9d82a4caeff5a57b78e60de307175c3634caa
Caitlin Selca <caitlinselca@gmail.com>
2020-02-05 22:18:21 -0500

    added comments

--- content_scripts/animalsandcolors.js
+++ content_scripts/animalsandcolors.js
@@ -1,6 +1,6 @@
 (function() {
 
   document.body.style.border = "50px solid purple"; //adds the purple border
-  document.body.style.backgroundImage = "url('https://pbs.twimg.com/profile_images/1032477983355760640/R1oLnD2P_400x400.jpg')"; //adds the repeated background image
+  document.body.style.backgroundImage = "url('https://pbs.twimg.com/profile_images/1032477983355760640/R1oLnD2P_400x400.jpg')"; //adds the repeated background image of the 8-bit cat
 
 })();
